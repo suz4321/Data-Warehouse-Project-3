66f44c57cc75f5068d83fe71a0ef5d8fddd8a28f
Suz4321 <sue.sheppard.21@gmail.com>
2021-04-20 10:59:57 -0600

    Update proj 3 etl.py
    final copy

--- proj 3 etl.py
+++ proj 3 etl.py
@@ -7,14 +7,12 @@ from sql_queries import copy_table_queries, insert_table_queries
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
-        print('load table insert')
         conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
-        print('table insert')
         conn.commit()
 
 
@@ -24,15 +22,10 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    print('We have a connection')
     
-    print('Call function load_staging_tables')
     load_staging_tables(cur, conn)
-    
-    print('Call function insert_tables')
     insert_tables(cur, conn)
     
-    print('loading complete')
     conn.close()
 
 if __name__ == "__main__":
